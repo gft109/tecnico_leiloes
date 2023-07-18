1a08fb1e15a3400de38673cca4d94ee926a86316
gft109 <gft1090@gmail.com>
2023-07-18 18:07:40 -0300

    mudei o nome

--- src/cadastroVIEW.java
+++ src/cadastroVIEW.java
@@ -39,7 +39,7 @@ public class cadastroVIEW extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Lucida Fax", 0, 24)); // NOI18N
-        jLabel1.setText("Sistema de Leilões");
+        jLabel1.setText("Grande Sistema de Leilões");
 
         jLabel3.setText("Cadastre um novo produto");
 
