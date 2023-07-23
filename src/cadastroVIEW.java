d700f5c61d226f45257e4831722f7c3cc7190715
Gustavo Ferreira <68303698+gft109@users.noreply.github.com>
2023-07-23 02:03:07 -0300

    CadastroVIEW - Mensagem de sucesso ao cadastrar

--- src/cadastroVIEW.java
+++ src/cadastroVIEW.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -149,7 +152,13 @@ public class cadastroVIEW extends javax.swing.JFrame {
         produto.setStatus(status);
         
         ProdutosDAO produtodao = new ProdutosDAO();
-        produtodao.cadastrarProduto(produto);
+        int resul = produtodao.cadastrarProduto(produto);
+        if(resul == 1){
+            JOptionPane.showMessageDialog(null, " Cadastro realizado com sucesso! ");
+        } 
+        else{
+           JOptionPane.showMessageDialog(null, " Erro ao cadastrar "); 
+        }
         
     }//GEN-LAST:event_btnCadastrarActionPerformed
 
