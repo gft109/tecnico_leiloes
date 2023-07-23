0cbf4e921e193ead31446ecb15e3b09753514a8b
Gustavo Ferreira <68303698+gft109@users.noreply.github.com>
2023-07-23 01:56:12 -0300

    Correção de bug em conectaDAO
    faltava colocar uma time zone

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -4,25 +4,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
     
     public Connection connectDB(){
         Connection conn = null;
         
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=mysql");
+            String url = "jdbc:mysql://localhost/uc11?user=root&password=mysql&serverTimezone=America/Sao_Paulo";
+            conn = DriverManager.getConnection(url);
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
